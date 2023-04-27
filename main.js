136c5a91a16104daccdc7b3fd2a241cd1f314a5a
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2023-04-27 10:12:45 +0400

    Update main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+// შევქმნათ რაიმე მასივი და მასში შექვმნათ რამდენიმე მასივი კიდევ სადაც თითოეულში ეწერება მაგალითად პროგრამული ენები როგორც გვქონდა წინა დავალებაზე
+//  სახელები გაკეთებული.
+//  კონსოლში მინდა რომ დაიბეჭდოს რომ თუ კონკრეტულად javascript იპოვის მასივიდან დამიწეროს რომ javascript found 
+//  სხვა ენებზე კი დამიბეჭდოს looking for და იმ ენის სახელები რაც მასივში გვაქვს.
+
+
+
+// const programmingLanguages = [["Python", "Java"], ["Dart", "JavaScript"],["Kotlin"]];
+
+// for (let i = 0; i < programmingLanguages.length; i++) {
+//   for (let j = 0; j < programmingLanguages[i].length; j++) {
+//     if (programmingLanguages[i][j] === "JavaScript") {
+//       console.log("JavaScript found");
+//     } 
+//     else {
+//         console.log("Looking for " + programmingLanguages[i]);
+//       }
+//   }
+// }
